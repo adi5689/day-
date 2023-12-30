5343ded618a81741477414e6f9435f0cbd8d5737
Aditya <111190263+adi5689@users.noreply.github.com>
2023-12-31 00:06:33 +0530

    Update index.js

--- src/components/CredStory/index.js
+++ src/components/CredStory/index.js
@@ -19,7 +19,7 @@ const CredStory = () => {
           <div className="story-para">
             But the truth is, we have been through lows and highs in our journey,had many hurdles but what made us powerful was we sticked to each other, which I&apos;m really proud of.
             Honestly, I didn&apos;t think we will come this far in the beginning but now that we&apos;re here, I really enjoyed the journey and want to share the same in the future with you.
-            So proud of how you have grown as a lady and als how you made me a better man. And honestly if I&apos;ll say, you&apos;re my <strong>&apos;Happy Place&apos;</strong>, my <strong>&apos;Sukoon&apos;</strong>, my <strong>&apos;Home&apos;</strong>.
+            So proud of how you have grown as a lady and als how you made me a better man. And honestly if I&apos;ll say, you&apos;re you&apos;re <strong>&apos;Monica&apos;</strong> to my <strong>&apos;Chandler&apos;</strong>, my <strong>&apos;Happy Place&apos;</strong>, my <strong>&apos;Sukoon&apos;</strong>, my <strong>&apos;Home&apos;</strong>.
           </div>
           <div className="story-para">
             Cheers to making many more of stories that only we know and have a blast with the cute little <strong>&apos;Chirkoot Monkey&apos;</strong> that I&apos;m with.<br />
